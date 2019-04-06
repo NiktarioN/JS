10cb744bd8513471ac4a6479e2049c528a2aa48d
Nikita <nikita_givchik@mail.ru>
2019-04-06 13:00:52 +0300

    Загрузил последние правки

--- lesson_1add/js/main.js
+++ lesson_1add/js/main.js
@@ -2,3 +2,14 @@
 
 let num = 33721;
 
+let numMultiplication = num.toString().split('').reduce(function(a,b){
+	return +a * +b;
+});
+
+console.log(numMultiplication);
+
+let pow = numMultiplication ** 3;
+
+alert("Первые две цифры полученного числа после возведения в степень: " + pow.toString().substring(0, 2));
+
+console.log(pow);
